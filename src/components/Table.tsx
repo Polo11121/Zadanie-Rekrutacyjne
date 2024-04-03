191d9eb60c378f66eec233f63ea4f0cd9758b87e
michal <michal.jasinski11121@gmail.com>
2024-04-03 13:44:33 +0200

    fix styling

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -12,19 +12,21 @@ export const Table = ({ headers, rows, isStriped = false }: TableProps) => (
   <FlowbiteTable striped={isStriped}>
     <FlowbiteTable.Head>
       {headers.map((header) => (
-        <FlowbiteTable.HeadCell key={header}>{header}</FlowbiteTable.HeadCell>
+        <FlowbiteTable.HeadCell className="flex-1" key={header}>
+          {header}
+        </FlowbiteTable.HeadCell>
       ))}
     </FlowbiteTable.Head>
     <FlowbiteTable.Body className="divide-y">
       {rows.map((cells, index) => (
         <FlowbiteTable.Row
           key={index}
-          className="bg-white dark:border-gray-700 dark:bg-gray-800"
+          className="bg-white dark:border-gray-700 dark:bg-gray-800 flex w-full"
         >
           {cells.map((cell) => (
             <FlowbiteTable.Cell
               key={cell}
-              className="whitespace-nowrap font-medium text-gray-900 dark:text-white"
+              className="whitespace-nowrap font-medium text-gray-900 dark:text-white flex-1"
             >
               {cell}
             </FlowbiteTable.Cell>
